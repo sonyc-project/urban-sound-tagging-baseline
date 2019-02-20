782ea200930911656a277298a688d0a2d543b025
Ho-Hsiang Wu <hohsiangwu@Ho-Hsiangs-MacBook-Pro.local>
2019-02-20 14:29:16 -0500

    Remove all the sonyc embedding

--- urban-sound-tagging-baseline/extract_embedding_vggish.py
+++ urban-sound-tagging-baseline/extract_embedding_vggish.py
@@ -7,12 +7,12 @@ from tqdm import tqdm
 import keras
 import tensorflow as tf
 from kapre.time_frequency import Melspectrogram
+
+
 from .vggish import vggish_input
 from .vggish import vggish_postprocess
 from .vggish import vggish_slim
 
-from ..models import MODELS
-from .extract_embedding import extract_embedding as extract_sonyc_embedding
 from .sonyc_data import load_sonyc_data
 
 TARGET_DURATION = 1.0  # seconds
@@ -44,7 +44,7 @@ def make_extract_vggish_embedding(frame_duration, hop_duration, input_op_name='v
     }
 
     if not resources_dir:
-        resources_dir = os.path.join(os.path.dirname(__file__), '../../resources/vggish')
+        resources_dir = os.path.join(os.path.dirname(__file__), 'vggish/resources')
 
     pca_params_path = os.path.join(resources_dir, 'vggish_pca_params.npz')
     model_path = os.path.join(resources_dir, 'vggish_model.ckpt')
@@ -85,10 +85,9 @@ def make_extract_vggish_embedding(frame_duration, hop_duration, input_op_name='v
         pass
 
 
-def extract_embeddings_sonyc(annotation_path, audio_dir, output_dir, exp_id, sr, emb_type, model_path=None,
-                             model_input_type=None, subnetwork_model_name=None, frame_duration=1.0, hop_duration=0.1,
-                             progress=True, vggish_resource_dir='/home/jtc440/dev/l3embedding/resources/vggish',
-                             vggish_embedding_size=128):
+def extract_embeddings_vggish(annotation_path, audio_dir, output_dir, exp_id, frame_duration=1.0, hop_duration=0.1,
+                              progress=True, vggish_resource_dir='/home/jtc440/dev/l3embedding/resources/vggish',
+                              vggish_embedding_size=128):
     """
     Extract embeddings for files annotated in the SONYC annotation file and save them to disk.
 
@@ -98,11 +97,6 @@ def extract_embeddings_sonyc(annotation_path, audio_dir, output_dir, exp_id, sr,
     audio_dir
     output_dir
     exp_id
-    sr
-    emb_type
-    model_path
-    model_input_type
-    subnetwork_model_name
     frame_duration
     hop_duration
     progress
@@ -116,35 +110,13 @@ def extract_embeddings_sonyc(annotation_path, audio_dir, output_dir, exp_id, sr,
 
     file_list = load_sonyc_data(annotation_path)[0]
 
-    frame_length = int(frame_duration * sr)
-    hop_length = int(hop_duration * sr)
+    extract_vggish_embedding = make_extract_vggish_embedding(frame_duration, hop_duration,
+        input_op_name='vggish/input_features', output_op_name='vggish/embedding',
+        resources_dir=vggish_resource_dir, embedding_size=vggish_embedding_size)
+    # Start coroutine
+    next(extract_vggish_embedding)
 
-    if emb_type == 'vggish':
-        assert sr == 16000
-
-        extract_vggish_embedding = make_extract_vggish_embedding(frame_duration, hop_duration,
-            input_op_name='vggish/input_features', output_op_name='vggish/embedding',
-            resources_dir=vggish_resource_dir, embedding_size=vggish_embedding_size)
-        # Start coroutine
-        next(extract_vggish_embedding)
-
-    elif emb_type == 'sonyc':
-        # load model and extract embedding subnet
-        assert model_path is not None
-        assert subnetwork_model_name is not None
-        assert sr == 48000
-
-        target_length = int(np.ceil(sr * TARGET_DURATION))
-
-        model = keras.models.load_model(
-            model_path, custom_objects={'Melspectrogram': Melspectrogram})
-        emb_model, _, _, _ = MODELS[subnetwork_model_name]()
-        emb_model.set_weights(model.get_layer('resnet50').get_weights())
-
-    else:
-        raise ValueError('Invalid embedding type: {}'.format(emb_type))
-
-    out_dir = os.path.join(output_dir, exp_id, emb_type)
+    out_dir = os.path.join(output_dir, exp_id, 'vggish')
     os.makedirs(out_dir, exist_ok=True)
 
     if progress:
@@ -153,15 +125,9 @@ def extract_embeddings_sonyc(annotation_path, audio_dir, output_dir, exp_id, sr,
     for filename in file_list:
         audio_path = os.path.join(audio_dir, filename)
         emb_path = os.path.join(out_dir, os.path.splitext(filename)[0] + '.npy.gz')
+        extract_vggish_embedding.send((audio_path, emb_path))
 
-        if emb_type == 'sonyc':
-            extract_sonyc_embedding(audio_path, emb_path, emb_model, model_input_type, sr=sr,
-                                    frame_length=frame_length, hop_length=hop_length, target_length=target_length)
-        elif emb_type == 'vggish':
-            extract_vggish_embedding.send((audio_path, emb_path))
-
-    if emb_type == 'vggish':
-        extract_vggish_embedding.close()
+    extract_vggish_embedding.close()
 
 
 if __name__ == "__main__":
@@ -173,9 +139,6 @@ if __name__ == "__main__":
     parser.add_argument("outputfolder")
     parser.add_argument("expid")
 
-    parser.add_argument("--modelpath")
-    parser.add_argument("--modelinputtype")
-    parser.add_argument("--subnetwork_model_name")
     parser.add_argument("--vggish_resource_dir")
     parser.add_argument("--vggish_embedding_size", type=int, default=128)
 
@@ -186,17 +149,13 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    extract_embeddings_sonyc(annotation_path=args.annpath,
-                             audio_dir=args.audiodir,
-                             emb_type=args.embtype,
-                             output_dir=args.outputfolder,
-                             model_path=args.modelpath,
-                             model_input_type=args.modelinputtype,
-                             exp_id=args.expid,
-                             subnetwork_model_name=args.subnetwork_model_name,
-                             vggish_resource_dir=args.vggish_resource_dir,
-                             vggish_embedding_size=args.vggish_embedding_size,
-                             sr=args.sr,
-                             frame_duration=args.frame_duration,
-                             hop_duration=args.hop_duration,
-                             progress=args.progress)
+    extract_embeddings_vggish(annotation_path=args.annpath,
+                              audio_dir=args.audiodir,
+                              output_dir=args.outputfolder,
+                              exp_id=args.expid,
+                              vggish_resource_dir=args.vggish_resource_dir,
+                              vggish_embedding_size=args.vggish_embedding_size,
+                              sr=args.sr,
+                              frame_duration=args.frame_duration,
+                              hop_duration=args.hop_duration,
+                              progress=args.progress)
---
+++ urban-sound-tagging-baseline/sonyc_data.py
@@ -0,0 +1,118 @@
+import numpy as np
+import csv
+from collections import OrderedDict
+
+taxonomy = OrderedDict([
+    ('engine', ['smallsoundingengine',
+               'mediumsoundingengine',
+               'largesoundingengine',
+               'engineofuncertainsize']),
+
+    ('impactsound', ['rockdrill',
+                    'jackhammer',
+                    'hoeram',
+                    'piledriver',
+                    'otherunknownimpactmachinery',
+                    'nonmachineryimpactsound']),
+
+    ('poweredsaw', ['chainsaw',
+                   'smallmediumrotatingsaw',
+                   'largerotatingsaw',
+                   'otherunknownpoweredsaw']),
+
+    ('alertsignal', ['carhorn',
+                    'caralarm',
+                    'siren',
+                    'reversebeeper',
+                    'otherunknownalertsignal']),
+
+    ('music', ['stationarymusic',
+              'mobilemusic',
+              'icecreamtruck',
+              'musicfromuncertainsource']),
+
+    ('humanvoice', ['personorsmallgrouptalking',
+                   'personorsmallgroupshouting',
+                   'largecrowd',
+                   'amplifiedspeech',
+                   'otherunknownhumanvoice']),
+
+    ('dogbarkingwhining', ['dogbarkingwhining']),
+
+    ('sensorfault', ['sensorfault']),
+
+    ('otherunknownconstructionsound', ['otherunknownconstructionsound'])
+])
+
+
+LOW_LEVEL_LABELS = [y for x in taxonomy.values() for y in x]
+HIGH_LEVEL_LABELS = list(taxonomy.keys())
+
+
+def load_sonyc_data(annotation_path):
+    """
+    Load SONYC annotation data from an annotation file.
+
+    Parameters
+    ----------
+    annotation_path
+
+    Returns
+    -------
+    file_list
+    high_target_list
+    low_target_list
+    train_idxs
+    eval_idxs
+
+    """
+    file_set = set()
+    file_list = []
+    train_idxs = []
+    eval_idxs = []
+    high_level_anns = {}
+    low_level_anns = {}
+
+    with open(annotation_path, 'r') as f:
+        reader = csv.DictReader(f)
+
+        idx = 0
+        for row in reader:
+            filename = row['audio_filename']
+            if filename not in file_set:
+                file_set.add(filename)
+                file_list.append(filename)
+                if row['split'] == 'train':
+                    train_idxs.append(idx)
+                else:
+                    eval_idxs.append(idx)
+
+                low_level_anns[filename] = []
+                high_level_anns[filename] = []
+                idx += 1
+
+            low_distr = []
+            for low_label in LOW_LEVEL_LABELS:
+                val = float(row['low_' + low_label + '_presence'])
+                low_distr.append(val)
+            low_distr = np.clip(np.array(low_distr), 0, 1)
+            low_level_anns[filename].append(low_distr)
+
+
+            high_distr = []
+            for high_label in HIGH_LEVEL_LABELS:
+                val = float(row['high_' + high_label + '_presence'])
+                high_distr.append(val)
+            high_distr = np.clip(np.array(high_distr), 0, 1)
+            high_level_anns[filename].append(high_distr)
+
+    high_target_list = []
+    low_target_list = []
+
+    for filename in file_list:
+        low_target = np.array(low_level_anns[filename]).mean(axis=0)
+        low_target_list.append(low_target)
+        high_target = np.array(high_level_anns[filename]).mean(axis=0)
+        high_target_list.append(high_target)
+
+    return file_list, high_target_list, low_target_list, train_idxs, eval_idxs
