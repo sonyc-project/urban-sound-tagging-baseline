3eaa59a65380f5c8cc2b8c8e69f6b1434585e2bf
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2019-03-07 15:42:58 -0500

    use mu instead of 0.5 literal

--- urban-sound-tagging-baseline/metrics.py
+++ urban-sound-tagging-baseline/metrics.py
@@ -312,8 +312,7 @@ def evaluate(prediction_path, annotation_path, yaml_path, mode):
         # and therefore a numerator of exactly zero. Therefore, any additive
         # offset mu would do as long as 0 < mu < 1. Choosing mu = 0.5 is
         # purely arbitrary and has no effect on the outcome (i.e. zero).
-        mu = 0.5
-        eval_df["P"] = TPs / np.maximum(TPs + FPs, 0.5)
+        eval_df["P"] = TPs / np.maximum(TPs + FPs, mu)
 
         # Likewise for recalls, although this numerical safeguard is probably
         # less necessary given that TP+FN=0 implies that there are zero
